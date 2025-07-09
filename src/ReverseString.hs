1c01cfd6a698c0a08ecc13817f6984f3968db770
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-09 09:27:51 +0200

    .

--- src/ReverseString.hs
+++ src/ReverseString.hs
@@ -1,4 +1,7 @@
 module ReverseString (reverseString) where
 
 reverseString :: String -> String
-reverseString = reverse
+reverseString s = rev s []
+  where
+    rev [] a = a
+    rev (x : xs) a = rev xs (x : a)
